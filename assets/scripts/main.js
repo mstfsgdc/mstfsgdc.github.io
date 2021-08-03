418e55e5e1da6bd48760067cbb1369c0bbefa10e
Mustafa Sagdic <mmsagdic@gmail.com>
2021-08-03 18:51:39 +0300

    first commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: ['Hello, World!', 'Merhaba, Dünya!'],
+    typeSpeed: 200,
+    // loop: true,
+    // loopCount: Infinity,
+    showCursor: false,
+    fadeOut: true
+}
+var typed = new Typed('.text', options);
