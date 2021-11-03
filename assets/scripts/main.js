f9210b43e0b0b35d2865d42a9418ce5f864c58a2
Mustafa <mmsagdic@gmail.com>
2021-11-03 20:30:22 +0300

    Delete assets directory

--- assets/scripts/main.js
+++
@@ -1,9 +0,0 @@
-var options = {
-    strings: ['Hello, World!', 'Merhaba, Dünya!'],
-    typeSpeed: 200,
-    // loop: true,
-    // loopCount: Infinity,
-    showCursor: false,
-    fadeOut: true
-}
-var typed = new Typed('.text', options);
