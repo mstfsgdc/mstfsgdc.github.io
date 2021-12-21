0f7e36c5e92c774dd1d9f84266fe3f0cc76da721
Mustafa <mmsagdic@gmail.com>
2021-12-21 04:14:41 +0300

    Change "safak" date

---
+++ precache-manifest.3e9892e4664d9c553f34ce2af1bf7d04.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce81bca3cb7e150a9ce0ceb921001bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bafb3d2891e94b4101e9",
+    "url": "/static/css/main.e95a87e3.chunk.css"
+  },
+  {
+    "revision": "249515675d7deb8e993a",
+    "url": "/static/js/2.4e4cf58c.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/static/js/2.4e4cf58c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafb3d2891e94b4101e9",
+    "url": "/static/js/main.1369b0fb.chunk.js"
+  },
+  {
+    "revision": "5e05a154e118b2d6e79d",
+    "url": "/static/js/runtime-main.c7402094.js"
+  },
+  {
+    "revision": "cf5c80e9dfe5fb64bb52fdd013c127d2",
+    "url": "/static/media/education-svgrepo-com.cf5c80e9.svg"
+  },
+  {
+    "revision": "d453e41653379c6e3730f4fa6b597d50",
+    "url": "/static/media/photo.d453e416.png"
+  },
+  {
+    "revision": "8e26f22094a11f6a689d8302dc30782c",
+    "url": "/static/media/react.8e26f220.svg"
+  },
+  {
+    "revision": "bf6f46710534592562d79d359a200d3a",
+    "url": "/static/media/science-svgrepo-com.bf6f4671.svg"
+  },
+  {
+    "revision": "e1f7813a81c4579705af8aec19b9aac0",
+    "url": "/static/media/twitch-seeklogo.com.e1f7813a.svg"
+  },
+  {
+    "revision": "1615e4b547323f089792331c6d4777a6",
+    "url": "/static/media/youtube-seeklogo.com.1615e4b5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16e2f733f71aecef4440780fbf591429.js"
+  "/precache-manifest.3e9892e4664d9c553f34ce2af1bf7d04.js"
 );
 
 self.addEventListener('message', (event) => {
